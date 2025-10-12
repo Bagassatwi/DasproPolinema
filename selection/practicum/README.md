6e888577168300015c03d18a4b683827c5a7cc6f
Gamer2Language <bagassatwi@gmail.com>
2025-10-12 18:29:34 +0700

    Attempt 1

---
+++ selection/practicum/README.md
@@ -0,0 +1,20 @@
+# Selection Practicum
+
+Simple overview of use/purpose.
+
+## Description
+
+Basic Programming Practicum: Selection Assigment
+
+## Getting Started
+
+### Question 2.1
+1. Why doesn't the check in the IF structure involve conditions with relational operators?
+   
+   Be
+2. When the program is run, then you enter the value false , what is the result?
+3. The system needs to provide information that if the user enters a false value , the output
+will be "Registration rejected. Please pay UKT first." Modify the program by adding an
+ELSE!
+1. Commit and push your modifications to Github with the message “Test Modification
+1”
---
+++ selection/practicum/ifCetakKRS06.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gamer2Language
+ */
+public class ifCetakKRS06 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("---Print KRS SIAKAD---");
+        System.out.print("Have the tutition fees paid in full? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+        if (uktLunas) {
+            System.out.println("UKT Payment has been verified");
+            System.out.println("KRS now can be printed and you can ask the academix advisor to sign it.");
+        }
+    }
+}
