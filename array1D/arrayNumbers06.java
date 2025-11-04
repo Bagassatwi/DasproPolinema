2070d93b40a4a431b70ae878cfe82b484a72757c
Gamer2Language <bagassatwi@gmail.com>
2025-11-04 07:23:37 +0700

    feat: add arrayNumbers06

---
+++ array1D/arrayNumbers06.java
@@ -0,0 +1,13 @@
+public class arrayNumbers06 {
+  public static void main(String[] args) {
+    int[] num = new int[5];
+    num[0] = 5;
+    num[1] = 12;
+    num[2] = 7;
+    num[3] = 20;
+    System.out.println(num[0]);
+    System.out.println(num[1]);
+    System.out.println(num[2]);
+    System.out.println(num[3]);
+  }
+}
