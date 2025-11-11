cade1485680576b13f542512263af2ba3d2f03c5
Gamer2Language <bagassatwi@gmail.com>
2025-11-11 09:16:16 +0700

    feat: add ragged array

---
+++ Array_2/Number06.java
@@ -0,0 +1,10 @@
+package Array_2;
+
+public class Number06 {
+  public static void main(String[] args) {
+    int[][] myNumbers = new int[3][];
+    myNumbers[0] = new int[5];
+    myNumbers[0] = new int[3];
+    myNumbers[0] = new int[1];
+  }
+}
